203fe15a534dbe49166d1303367f289a78be9896
Lonozx <lonozx@gmail.com>
2023-02-21 17:52:23 +0200

    logo added

--- front/src/common/Header/Header.jsx
+++ front/src/common/Header/Header.jsx
@@ -1,11 +1,11 @@
 import './header.scss'
-// import logo from '../common/imgs/Logo.png';
+import logo from '../imgs/Logo.png';
 
 export default function Header(props){
     return(
         <header className="header">
         <div className="container">
-            {/* <img className='logo' src={logo} alt="" /> */}
+            <img className='logo' src={logo} alt="" />
             {/* <h2 className="container__title">Header</h2> */}
             <img className='container__avatar' src={props.url} alt="avatar" />
         </div>
