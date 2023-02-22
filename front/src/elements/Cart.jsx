733b99c54dacf6b0916f981b6818541f1f6a4e3c
Lonozx <lonozx@gmail.com>
2023-02-22 21:35:57 +0200

    minor changes on array

--- front/src/elements/Cart.jsx
+++ front/src/elements/Cart.jsx
@@ -1,4 +1,5 @@
 import './cart.scss'
+import Main from '../sections/Main'
 
 export default function Cart(props){
     const tickets =[
@@ -13,6 +14,7 @@ export default function Cart(props){
         <div className="container__cart">
     <h2 className='container__cart-title'>{props.title}</h2>
     <p className='container__cart-desc'>{props.description}</p>
+    <p className='container__cart-desc'>{props.officeNumber}</p>
     <div className="button-section">
     <button className='cart-deny'>Deny</button>
     <button className='cart-apply'>Apply</button>
--- front/src/sections/Main.jsx
+++ front/src/sections/Main.jsx
@@ -1,7 +1,62 @@
+import React, { useState, useEffect  } from 'react';
+import Cart from '../elements/Cart';
 import './mainSection.scss';
 
 export default function Main(props){
+    const [showCart, setShowCart] = useState(false);
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [office, setOffice] = useState('');
+    const [state, setState] = useState(true);
+    const [data, setData] = useState([{}]);
+
+    useEffect(()=>{
+        setData({title, description, office});
+    }, [title, description, office]);
+
+    const handleTitle = (event) => {
+        setTitle(event.target.value);
+    }
+    const handleDescription = (event) => {
+        setDescription(event.target.value);
+    }
+    const handleOffice = (event) => {
+        setOffice(event.target.value);
+    }
+    const handleState = (event) => {
+        setState(event.target.value);
+    }
+    // const handleData = () => {
+    //     const newData = {title, description, office};
+    //     setData(newData);
+    //     setTitle('');
+    //     setDescription('');
+    //     setOffice('');
+    // }
+
+    // const inputChange = (event)=>{
+    //     setInputValue(event.target.value);
+    // }
+
+    const handle = () =>{
+        setShowCart(true);
+        const newData = {title, description, office};
+        setData(newData);
+        setTitle('');
+        setDescription('');
+        setOffice('');
+        setState(true);
+    }
    return(
-    <button className="cart-create">Create a ticket!</button>
+    <>
+    <div className="input-data">
+    <input type='text' value={title} onChange={handleTitle} className='ticket title-ticket' placeholder='Title please'></input>
+    <input type='text' value={description} onChange={handleDescription} className='ticket description-ticket'placeholder='Here is your description'></input>
+    <input type='number' value={office} onChange={handleOffice} className='ticket office-ticket' placeholder='# of office'></input>
+    </div>
+    <button onClick={handle} className="cart-create">Create a ticket!</button>
+    {showCart && <Cart title={title} description={description} officeNumber={office}/>}
+    <pre>{JSON.stringify(data, null, 2)}</pre>  
+    </>
    )
 }
